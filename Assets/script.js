dec4f0b342e66a86a4d83abe9e021a04f08fe129
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-02-24 00:05:27 -0800

    Fixed contact number

--- Assets/script.js
+++ Assets/script.js
@@ -4,18 +4,15 @@
 
 //Get the button:
 
-$("#Phone").click(function(){
-  if(true){
-    $("#number").append(" (925) 699-3988");
-  }
-  
-});
+
+
 
 function myFunction() {
   var x = document.getElementById("number");
   if (x.innerHTML === " Contact number") {
-    x.innerHTML = " Contact number: (925) 699-3988";
+    x.innerHTML = " (925) 699-3988";
   } else {
     x.innerHTML = " Contact number";
   }
 }
+
