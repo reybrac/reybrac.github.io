e0d9bed88fca2088f160254c81de7f113053812c
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-04-17 12:13:50 -0700

    Fixed styling and added alt to images and links

--- Assets/script.js
+++ Assets/script.js
@@ -6,10 +6,10 @@
 
 function myFunction() {
   var x = document.getElementById("number");
-  if (x.innerHTML === " Contact number") {
+  if (x.innerHTML === " ") {
     x.innerHTML = " (925) 699-3988";
   } else {
-    x.innerHTML = " Contact number";
+    x.innerHTML = " ";
   }
 }
 
