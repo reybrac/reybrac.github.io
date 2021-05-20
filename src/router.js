166e5faa77991e32a20f114633f44dae98e82323
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-05-20 11:41:01 -0700

    Updating route to fix routing issue on deployed site

--- src/router.js
+++ src/router.js
@@ -2,19 +2,21 @@ import Homepage from "./pages/Homepage";
 import NoMatch from "./pages/NoMatch";
 import Portfolio from "./pages/Portfolio";
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  withRouter,
+} from "react-router-dom";
 
 function AppRouter() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/">
-            <Homepage />
-          </Route>
-          <Route exact path="/portfolio">
-            <Portfolio />
-          </Route>
+          <Route exact path="/" component={withRouter(Homepage)} />
+
+          <Route exact path="/portfolio" component={withRouter(Portfolio)} />
 
           <Route>
             <NoMatch />
