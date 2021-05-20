aaddbb34066733f3d920643c898a7d90995d002b
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-05-19 22:19:00 -0700

    Fixed the routing. Deployed site was not showing portfolio page.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import AppRouter from "./router";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
-// import Homepage from "./components/Homepage";
+import Homepage from "./pages/Homepage";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <AppRouter />
+      <AppRouter>
+        <Homepage />
+        <Portfolio />
+      </AppRouter>
       <Card />
     </div>
   );
