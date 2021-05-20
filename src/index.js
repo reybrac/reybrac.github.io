9fccc8f1f3db814b95ec3cad03965d2828d9c0b2
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-05-20 11:08:43 -0700

    Removed unnecessary router in index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,4 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
