2f730eda253088d597cff58a81cc336ade5b8c1e
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-05-20 09:52:44 -0700

    Updated App.js file

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <AppRouter>
-        <Homepage />
-        <Portfolio />
-      </AppRouter>
+      <AppRouter />
       <Card />
     </div>
   );
